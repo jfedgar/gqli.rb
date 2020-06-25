11e15618da71db41a90856127b49c65ab07d3d5f
jfedgar <jfedgar@gmail.com>
2020-06-25 01:34:36 -0500

    Return errors array in response if 'errors' are returned instead of 'data'

--- lib/gqli/client.rb
+++ lib/gqli/client.rb
@@ -38,9 +38,12 @@ module GQLi
 
       fail "Error: #{http_response.reason}\nBody: #{http_response.body}" if http_response.status >= 300
 
-      data = JSON.parse(http_response.to_s)['data']
+      parsed_response = JSON.parse(http_response.to_s)
+      data = parsed_response['data']
+      errors = parsed_response.fetch('errors')
+      errors ||= data.fetch('errors', [])
 
-      Response.new(data, query)
+      Response.new(data, query, errors)
     end
 
     # Validates a query against the schema
